cebc4fe4132b52931a8971bfd22720b6ecaaa8a5
Zofuku <52409028+Zofuku@users.noreply.github.com>
2019-08-29 17:49:54 +0900

    Update config.ts

--- templates/plugins/config.ts
+++ templates/plugins/config.ts
@@ -5,7 +5,7 @@ const feeBase = 10000
 const feePer = 100
 const satellitesAddress = '0x764Fe0b6dF8575b30bCfd0c9Bb2A7ADb390b5359'
 const satellitesFeeRatio = 100
-const ownerAddress = satellitesAddress
+const ownerAddress = '0xB22D813bE873aFFc6C3b92BeCd87455cE2aFF54d'
 const ownerFeeRatio = 900
 
 const networkIdToInfura: { [networkId: number]: string } = {
@@ -24,13 +24,58 @@ const networkIdToAPI: { [networkId: number]: string } = {
 }
 
 const networkIdToTokens: { [networkId: number]: any[] } = {
-  1: [
+  1: {
+    {
+      contract: '0x273f7f8e6489682df756151f5525576e322d51a3',
+      symbol: 'MCHH',
+      name: 'MyCryptoHeroes:Hero'
+    },
+    {
+      contract: '0xdceaf1652a131f32a821468dc03a92df0edd86ea',
+      symbol: 'MCHE',
+      name: 'MyCryptoHeroes:Extensions'
+    },
+    {
+      contract: '0x617913Dd43dbDf4236B85Ec7BdF9aDFD7E35b340',
+      symbol: 'MCHL',
+      name: 'MyCryptoHeroes:Land'
+    },
+    {
+       contract: '0xf87e31492faf9a91b02ee0deaad50d51d56d5d4d',
+       symbol: 'LAND',
+       name: 'Decentraland LAND'
+     },
     {
       contract: '0xf5b0a3efb8e8e4c201e2a935f110eaaf3ffecb8d',
       symbol: 'AXIE',
       name: 'AXIE'
-    }
-  ],
+    },
+    {
+      contract: '0xfac7bea255a6990f749363002136af6556b31e04',
+      symbol: 'ENS',
+      name: 'Ether Name Service'
+    },
+    {
+      contract: '0x79986af15539de2db9a5086382daeda917a9cf0c',
+      symbol: 'CVPA',
+      name: 'Cryptovoxels Parcel'
+    },
+    {
+       contract: '0x8c9b261faef3b3c2e64ab5e58e04615f8c788099',
+       symbol: 'MLBCB',
+       name: 'MLB Champions'
+     },
+     {
+       contract: '0x67cbbb366a51fff9ad869d027e496ba49f5f6d55',
+       symbol: 'CSPL',
+       name: 'CryptoSpells'
+     },
+　　　{
+       contract: '0x4d3814D4DA8083b41861dEC2F45B4840e8b72d68',
+       symbol: 'CSCNFT',
+       name: 'CSCNFTFactory'
+     },
+}
   4: [
     {
       contract: '0x84f6261350151dc9cbf5b33c5354fe9a82166e26',
